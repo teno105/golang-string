5ac8f7f5521bceee3b65198d426fc6ce7c1be6fe
teno <teno@peppercon.co.kr>
2024-10-06 14:18:29 +0900

    실습2

--- cmd/golang-string/main.go
+++ cmd/golang-string/main.go
@@ -1,15 +1,17 @@
-// cmd/golang-string/main.go
 package main
 
 import "fmt"
 
 func main() {
-	// 큰따옴표로 묶으면 특수 문자가 동작합니다.
-    str1 := "Hello\t'World'\n"
+	// 큰따옴표에서 려러 줄을 표현하려면 \n을 사용
+    poet1 := "죽는 날까지 하늘을 우러러\n한 점 부끄럼이 없기를,\n잎새에 이는 바람에도\n나는 괴로워했다.\n"
 
-	// 백쿼트로 묶으면 특수 문자가 동작하지 않습니다.
-	str2 := `Go is "awesome"!\nGo is simple and\t'powerful'`
+	// 백쿼트에서 려러 줄 표현에 특수 문자가 필요 없음
+	poet2 := `죽는 날까지 하늘을 우러러
+한 점 부끄럼이 없기를,
+잎새에 이는 바람에도
+나는 괴로워했다.`
 
-    fmt.Printf(str1)
-	fmt.Printf(str2)
+    fmt.Printf(poet1)
+	fmt.Printf(poet2)
 }
